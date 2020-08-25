85ac6605f105d854a77e74fcfada027d89caa7d7
Nji Daniel <njid18753@gmail.com>
2020-08-25 05:19:27 +0100

    first commit

---
+++ dan2.py
@@ -0,0 +1,17 @@
+from tkinter import*
+
+root=Tk()
+
+topFrame= Frame(root)
+top.Frame.pack()
+bottomFrame=Frame(root)
+bottomframe.pack(side=BOTTOM)
+
+button1= Button(topFrame,text="Button1",fg="red")
+button2= Button(topFrame,text="Button2",fg="blue")
+button3= Button(topFrame,text="Button3",fg="green")
+button4= Button(topFrame,text="Button4",fg="purple")
+
+
+
+root.mainloop()
